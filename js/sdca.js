2a0a43e815629a13b9516da5a6c00a02c9dd4884
mvl22 <martin@lucas-smith.co.uk>
2021-11-07 19:58:02 +0000

    Add alpha UI, using the Mapboxgljs.LayerViewer library, including two API-based data layers

---
+++ js/sdca.js
@@ -0,0 +1,127 @@
+// SDCA implementation code
+
+/*jslint browser: true, white: true, single: true, for: true, unordered: true, long: true */
+/*global $, alert, console, window, osm2geo, layerviewer, jQuery */
+
+var sdca = (function ($) {
+	
+	'use strict';
+	
+	// Settings defaults
+	var _settings = {
+		
+		// CycleStreets API; obtain a key at https://www.cyclestreets.net/api/apply/
+		apiBaseUrl: 'https://api.cyclestreets.net',
+		apiKey: 'YOUR_API_KEY',
+		
+		// Mapbox API key
+		mapboxApiKey: 'YOUR_MAPBOX_API_KEY',
+		
+		// Initial lat/lon/zoom of map and tile layer
+		defaultLocation: {
+			latitude: 53.891,
+			longitude: -1.216,
+			zoom: 5.2
+		},
+		defaultTileLayer: 'mapboxstreets',
+		
+		// Default layers ticked
+		defaultLayers: [],
+		
+		// Icon size, set globally for all layers
+		iconSize: [38, 42],
+		
+		// Zoom position
+		zoomPosition: 'top-left',
+		
+		// Geolocation position
+		geolocationPosition: 'top-left',
+		
+		// Enable scale bar
+		enableScale: true
+	};
+	
+	// Layer definitions
+	var _layerConfig = {
+		
+		trafficcounts: {
+			apiCall: '/v2/trafficcounts.locations',
+			apiFixedParameters: {
+				groupyears: '1'
+			},
+			iconUrl: '/images/icons/icon_congestion_bad.svg',
+			lineColourField: 'car_pcu',	// #!# Fixme - currently no compiled all_motors_pcu value
+			lineColourStops: [
+				[40000, '#ff0000'],	// Colour and line values based on GMCC site
+				[20000, '#d43131'],
+				[10000, '#e27474'],
+				[5000, '#f6b879'],
+				[2000, '#fce8af'],
+				[0, '#61fa61']
+			],
+			lineWidthField: 'cycle_pcu',	// #!# Fixme - should be Daily cycles
+			lineWidthStops: [
+				[1000, 10],
+				[500, 8],
+				[100, 6],
+				[10, 4],
+				[0, 2]
+			],
+			popupHtml:	// Popup code thanks to https://hfcyclists.org.uk/wp/wp-content/uploads/2014/02/captions-html.txt
+				  '<p>Count Point {properties.id} on <strong>{properties.road}</strong>, a {properties.road_type}<br />'
+				+ 'Located in {properties.wardname} in {properties.boroughname}<br />'
+				+ '[macro:yearstable({properties.minyear}, {properties.maxyear}, cycles;p2w;cars;buses;lgvs;mgvs;hgvs;all_motors;all_motors_pcu, Cycles;P2W;Cars;Buses;LGVs;MGVs;HGVs;Motors;Motor PCU)]'
+				+ '<p><strong>{properties.maxyear} PCU breakdown -</strong> Cycles: {properties.cycle_pcu}, P2W: {properties.p2w_pcu}, Cars: {properties.car_pcu}, Buses: {properties.bus_pcu}, LGVs: {properties.lgv_pcu}, MGVs: {properties.mgv_pcu}, HGVs: {properties.hgv_pcu}</p>'
+				+ '</div>'
+		},
+		
+		planningapplications: {
+			apiCall: 'https://www.planit.org.uk/api/applics/geojson',
+			apiFixedParameters: {
+				pg_sz: 100,
+				limit: 100,
+				select: 'location,description,address,app_size,app_type,app_state,uid,area_name,start_date,url'
+			},
+			apiKey: false,
+			iconUrl: '/images/icons/signs_neutral.svg',
+			iconSizeField: 'app_size',
+			iconSizes: {
+				'Small': [24, 24],
+				'Medium': [36, 36],
+				'Large': [50, 50]
+			},
+			popupHtml:
+				  '<p><strong>{properties.description}</strong></p>'
+				+ '<p>{properties.address}</p>'
+				+ '<p>Size of development: <strong>{properties.app_size}</strong><br />'
+				+ 'Type of development: <strong>{properties.app_type}</strong><br />'
+				+ 'Status: <strong>{properties.app_state}</strong></p>'
+				+ '<p>Reference: <a href="{properties.url}">{properties.uid}</a><br />'
+				+ 'Local Authority: {properties.area_name}<br />'
+				+ 'Date: {properties.start_date}</p>'
+				+ '<p><a href="{properties.url}"><img src="/images/icons/bullet_go.png" /> <strong>View full details</a></strong></p>'
+		}
+	};
+	
+	
+	return {
+		
+	// Public functions
+		
+		// Main function
+		initialise: function (config)
+		{
+			// Merge the configuration into the settings
+			$.each (_settings, function (setting, value) {
+				if (config.hasOwnProperty(setting)) {
+					_settings[setting] = config[setting];
+				}
+			});
+			
+			// Run the layerviewer for these settings and layers
+			layerviewer.initialise (_settings, _layerConfig);
+		}
+	};
+	
+} (jQuery));
+
