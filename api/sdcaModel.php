7437c3d8fef2c5982fe35c0f784e303d6e09a2e8
mvl22 <martin@lucas-smith.co.uk>
2022-01-30 21:01:39 +0000

    Break out mock JSON into parts, ready for implementation of each block

--- api/sdcaModel.php
+++ api/sdcaModel.php
@@ -93,10 +93,44 @@ class sdcaModel
 		$parameter = file_get_contents ($_SERVER['DOCUMENT_ROOT'] . '/lexicon/example_r_input.json');
 		$parameter = str_replace ('D:/GitHub/SDCA-tool/sdca-data-prep/data/UKdem.tif', '/var/www/sdca/data/dem/UKdem.tif', $parameter);
 		$parameter = str_replace ('D:/GitHub/SDCA-tool/sdca-data-prep/data/landcover.tif', '/var/www/sdca/data/landcover/landcover.tif', $parameter);
+		$mockDataJson = json_decode ($parameter, true);
+		
+		# Construct the JSON to be sent to the API
+		$json = array ();
+		
+		# Values for user_input
+		$json['user_input'] = $mockDataJson['user_input'];
+		
+		# Values for intervention_assets
+		$json['intervention_assets'] = $mockDataJson['intervention_assets'];
+		
+		# Values for intervention_assets_parameters
+		$json['intervention_assets_parameters'] = $mockDataJson['intervention_assets_parameters'];
+		
+		# Values for asset_components
+		$json['asset_components'] = $mockDataJson['asset_components'];
+		
+		# Values for carbon_factors
+		$json['carbon_factors'] = $mockDataJson['carbon_factors'];
+		
+		# Values for desire_lines
+		$json['desire_lines'] = $mockDataJson['desire_lines'];
+		
+		# Value for path_dem file
+		$json['path_dem'] = $mockDataJson['path_dem'];
+		
+		# Value for path_landcover file
+		$json['path_landcover'] = $mockDataJson['path_landcover'];
+		
+		# Values for material_sites
+		$json['material_sites'] = $mockDataJson['material_sites'];
+		
+		# Construct as string
+		$stdin = json_encode ($json);
 		
 		# Provide base data to calculation script
 		$command = $_SERVER['DOCUMENT_ROOT'] . '/api/sdca.R';
-		$result = $this->createProcess ($command, $parameter);
+		$result = $this->createProcess ($command, $stdin);
 		$result = json_decode ($result, true);
 		
 		# Return the result
