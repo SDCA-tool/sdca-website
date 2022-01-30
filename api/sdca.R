a6eb0c994ebdf82bbf66ceda9506351ccf7cce8a
Malcolm Morgan <mem48@users.noreply.github.com>
2022-01-30 19:58:32 +0000

    switch to sdtin

--- api/sdca.R
+++ api/sdca.R
@@ -8,12 +8,12 @@ library(sdca)
 
 # Get data values from STDIN
 # See: https://datafireball.com/2013/10/10/putting-your-r-code-into-pipeline/comment-page-1/
-#input = file('stdin', 'r')
-#args = try(readLines(input, n=1, warn=FALSE), silent = TRUE)
+input = file('stdin', 'r')
+args = try(readLines(input, n=1, warn=FALSE), silent = TRUE)
 
 # Alt method from:
 # https://www.r-bloggers.com/2015/09/passing-arguments-to-an-r-script-from-command-lines/
-args = try(commandArgs(trailingOnly=FALSE), silent = TRUE)
+#args = try(commandArgs(trailingOnly=FALSE), silent = TRUE)
 
 # Check if the input arrived
 if("try-error" %in% class(args)){
