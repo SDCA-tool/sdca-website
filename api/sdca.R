0884062fc99e626606b4a61937d19d7f8134c8b5
Malcolm Morgan <mem48@users.noreply.github.com>
2022-01-31 21:28:23 +0000

    Clean up the main script

--- api/sdca.R
+++ api/sdca.R
@@ -11,50 +11,24 @@ library(sdca)
 input = file('stdin', 'r')
 args = try(readLines(input, warn=FALSE), silent = TRUE)
 
-
-# Alt method from:
-# https://www.r-bloggers.com/2015/09/passing-arguments-to-an-r-script-from-command-lines/
-#args = try(commandArgs(trailingOnly=FALSE), silent = TRUE)
-
 # Check if the input arrived
 if("try-error" %in% class(args)){
-  result = list(error = gsub("[\r\n]", "", result[1]))
-  result = jsonlite::toJSON(result)
-  cat(result)
+  cat(jsonlite::toJSON(list(error = gsub("[\r\n]", "", result[1]))))
 } else if (length(args) == 0){
-  result = list(error = "API returned empty result")
-  result = jsonlite::toJSON(result)
-  cat(result)
+  cat(jsonlite::toJSON(list(error = "API returned empty result")))
 } else {
-  #if(length(args) > 1){
-    args = paste(args, collapse = "")
-    #result = list(error = paste(substr(args,1,50), collapse = ", "))
-    #result = jsonlite::toJSON(result)
-    #cat(result)
-  #} else {
-    # Check if args contains a file path or json
-    if(nchar(args) < 100){
-      file = TRUE
-    } else {
-      file = FALSE
-    }
-    
-    # Process the data
-    result = try(
-      suppressMessages(suppressWarnings(process_results(args, file))), silent = TRUE)
+  args = paste(args, collapse = "")
+  
+  # Process the data
+  result = try(
+    suppressMessages(suppressWarnings(process_results(args, file = FALSE))), silent = TRUE)
 
-    # Check if the function worked
-    if("try-error" %in% class(result)){
-      result = list(error = gsub("[\r\n]", "", result[1]))
-      result = jsonlite::toJSON(result)
-    }
-    
-    # Print the result, without newlines or a count
-    cat(result)
-  #}
+  # Check if the function worked
+  if("try-error" %in% class(result)){
+    result = list(error = gsub("[\r\n]", "", result[1]))
+    result = jsonlite::toJSON(result)
+  }
+  
+  # Print the result, without newlines or a count
+  cat(result)
 }
-
-
-
-
-
